d4e171b0719052e9f44c0f228c07668d3c34fb4e
Michael Pham <phamminh1309@gmail.com>
2019-08-30 10:32:01 +0700

    Box Copy File
    BOX COPY FILE – ACTIVITY DESCRIPTION
    Creates a copy of a file in Box.
    
    BOX COPY FILE – OUTPUT
    Success/Failure
    
    BOX COPY FILE – ASSUMPTIONS
    1. This custom activity will be developed via Ayehu Activity Designer (see Activity Designer documentation).
    2. This custom activity will be utilizing the Box API.
    
    BOX COPY FILE – SETTINGS
    Image not exact, see details below for more info
    
    1. Once the user clicks on the activity settings, the above page will be presented as a default.
    
    2. The following fields are required and therefore should have a red * character next to the label:
    a. Folder ID – The ID of the destination folder.
    b. File ID – The ID of the file to copy.
    
    3. The following fields are optional:
    a. Version – An optional file version ID if you want to copy a specific file version.
    b. Name – An optional new name for the file.

---
+++ Box/BoxCopyFile.cs
@@ -0,0 +1,62 @@
+﻿using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System.IO;
+using Box.V2.Config;
+using Box.V2.JWTAuth;
+using Box.V2.Models;
+using System;
+
+namespace Ayehu.Sdk.Box
+{
+    public class CopyFile : IActivity
+    {
+        public string FolderID;
+        public string FileID;
+        public string Name;
+        public string Version;
+        public string USER_ID ;
+        public string CLIENT_ID;
+        public string CLIENT_SECRET;
+        public string ENTERPRISE_ID ;
+        public string JWT_PRIVATE_KEY_PASSWORD;
+        public string JWT_PUBLIC_KEY_ID ;
+        public string PRIVATE_KEY;
+        public ICustomActivityResult Execute()
+        {
+            var Message = string.Empty;
+            try
+            {
+                var privateKey = File.ReadAllText(PRIVATE_KEY);
+                var boxConfig = new BoxConfig(CLIENT_ID, CLIENT_SECRET, ENTERPRISE_ID, privateKey, JWT_PRIVATE_KEY_PASSWORD, JWT_PUBLIC_KEY_ID);
+                var boxJWT = new BoxJWTAuth(boxConfig);
+                var adminToken = boxJWT.UserToken(USER_ID);
+                var adminClient = boxJWT.AdminClient(adminToken);
+                var requestParams = new BoxFileRequest()
+                {
+                    Id = FileID,
+                    Parent = new BoxRequestEntity()
+                    {
+                        Id = FolderID
+                    },
+                    Name = Name
+                };
+
+                BoxFile fileCopy = await adminClient.FilesManager.CopyAsync(requestParams);
+                //if (string.IsNullOrEmpty(fileCopy.Id))
+                //{
+                //    result = "Can not copy file";
+                //}
+                //else
+                //{
+                //    result = "File is copied with id " + fileCopy.Id;
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                Message = "Copy Error";
+            }
+            return this.GenerateActivityResult(Message);
+        }
+    }
+}
