e778e10851a437cc7084af167a2beafa6b9831f4
João Carlos Florêncio <admin@bztex.com.br>
2019-10-04 12:47:19 -0300

    - Reoganizing folders; - Inserting 3 image activities; - Inserting 1 disk activity;

---
+++ Google Cloud/Image/GCloudDeleteImage/GCloudDeleteImage.cs
@@ -0,0 +1,56 @@
+﻿using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Google.Apis.Services;
+using Google.Apis.Compute.v1;
+using Google.Apis.Auth.OAuth2;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ActivitiesAyehu
+{
+    public class GCloudDeleteImage : IActivity
+    {
+        public string Project;
+        public string ImageName;
+        public string PrivateKey;
+        public string ServiceAccountEmail;
+
+
+        public ICustomActivityResult Execute()
+        {
+            var result = DeleteImage();
+
+            return this.GenerateActivityResult(result.Result);
+        }
+
+        private async Task<string> DeleteImage()
+        {
+            ServiceAccountCredential credential = new ServiceAccountCredential(
+               new ServiceAccountCredential.Initializer(ServiceAccountEmail)
+               {
+                   Scopes = new[] { ComputeService.Scope.Compute, ComputeService.Scope.CloudPlatform }
+               }.FromPrivateKey(PrivateKey));
+
+            var cs = new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GCloud Delete Image"
+            };
+
+            var t = new ComputeService(cs);
+
+            var request = t.Images.Delete(Project, ImageName);
+
+            var response = request.Execute();
+
+            if (response.HttpErrorStatusCode != null)
+            {
+                var errorStr = new StringBuilder();
+                foreach (var error in response.Error.Errors)
+                    errorStr.AppendLine(error.Code + " - " + error.Message);
+                return errorStr.ToString();
+            }
+            else return "Success";
+        }
+    }
+}
---
+++ Google Cloud/Image/GCloudListImages/GCloudListImages.cs
@@ -0,0 +1,59 @@
+﻿using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System;
+using System.Collections.Generic;
+using Google.Apis.Services;
+using Google.Apis.Compute.v1;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Compute.v1.Data;
+using System.Threading.Tasks;
+using System.Text;
+using System.Data;
+
+namespace ActivitiesAyehu
+{
+    public class GCloudListImages : IActivity
+    {
+        public string Project;
+        public string PrivateKey;
+        public string ServiceAccountEmail;
+
+
+        public ICustomActivityResult Execute()
+        {
+            var result = ListImages();
+
+            var dataTable = new DataTable("Image List", "ImageList");
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Family");
+            dataTable.Columns.Add("Status");
+            foreach (var img in result.Result)
+                dataTable.Rows.Add(img.Name, img.Family, img.Status);
+
+            return this.GenerateActivityResult(dataTable);
+        }
+
+        private async Task<IList<Image>> ListImages()
+        {
+            ServiceAccountCredential credential = new ServiceAccountCredential(
+               new ServiceAccountCredential.Initializer(ServiceAccountEmail)
+               {
+                   Scopes = new[] { ComputeService.Scope.Compute, ComputeService.Scope.CloudPlatform }
+               }.FromPrivateKey(PrivateKey));
+
+            var cs = new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GCloud Delete Instance"
+            };
+
+            var t = new ComputeService(cs);
+
+            var request = t.Images.List(Project);
+
+            var response = request.Execute();
+
+            return response.Items;
+        }
+    }
+}
