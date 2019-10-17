5c1a2679fe38dd412209c47ff4e43a45e7837c69
João Carlos Florêncio <joaocpflorencio@gmail.com>
2019-10-17 09:55:24 -0300

    - Adding Docker activities: Build Image, Delete Image, List Images and Push Image.

---
+++ Docker/DockerBuildImage/DockerBuildImage.cs
@@ -0,0 +1,43 @@
+﻿using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System;
+using Docker.DotNet;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ActivitiesAyehu
+{
+    public class DockerBuildImage : IActivity
+    {
+        public string RemoteDockerURI;
+        public string RemoteContext;
+        public string ImageName;
+
+        public ICustomActivityResult Execute()
+        {
+            var result = BuildImage();
+
+            return this.GenerateActivityResult(result);
+        }
+
+        private string BuildImage()
+        {
+            DockerClient client = new DockerClientConfiguration(
+                new Uri(RemoteDockerURI))
+                 .CreateClient();
+
+            var stream = new MemoryStream();
+
+            var response = client.Images.BuildImageFromDockerfileAsync(stream,
+                new Docker.DotNet.Models.ImageBuildParameters
+                {
+                    RemoteContext = RemoteContext,
+                    Tags = new List<string> { ImageName }
+                });
+
+            response.Wait();
+
+            return "Success";
+        }
+    }
+}
---
+++ Docker/DockerListImages/DockerListImages.cs
@@ -0,0 +1,52 @@
+﻿using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System;
+using System.Collections.Generic;
+using Google.Apis.Services;
+using Google.Apis.Compute.v1;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Compute.v1.Data;
+using System.Threading.Tasks;
+using System.Text;
+using System.Data;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace ActivitiesAyehu
+{
+    public class DockerListImages : IActivity
+    {
+        public string RemoteDockerURI;
+
+        public ICustomActivityResult Execute()
+        {
+            var result = ListImages();
+
+            var dataTable = new DataTable("Image List", "ImageList");
+            dataTable.Columns.Add("Id");
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Shared size");
+            dataTable.Columns.Add("Size");
+            dataTable.Columns.Add("Parent ID");
+            foreach (var img in result)
+                dataTable.Rows.Add(img.ID, img.RepoTags[0], img.SharedSize, img.Size,
+                    img.ParentID);
+
+            return this.GenerateActivityResult(dataTable);
+        }
+
+        private IList<ImagesListResponse> ListImages()
+        {
+            DockerClient client = new DockerClientConfiguration(
+                new Uri(RemoteDockerURI))
+                 .CreateClient();
+
+            var response = client.Images.ListImagesAsync(
+                new Docker.DotNet.Models.ImagesListParameters());
+
+            response.Wait();
+
+            return response.Result;
+        }
+    }
+}
