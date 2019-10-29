b5a2c2b485ef7b29d4011ec7fb85a1c83400b971
Michael Pham <phamminh1309@gmail.com>
2019-10-29 10:54:28 +0700

    Add files via upload

---
+++ Amazon S3/Create Bucket/AWSCreateBucket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Amazon;
+using Amazon.S3;
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using System.Net;
+
+namespace Ayehu.Sdk.AWSS3
+{
+    class AWSCreateBucket : IActivity
+    {
+        public string bucketName;
+        public string AccessKey;
+        public string SecretKey;
+        public static S3CannedACL cannedACL = S3CannedACL.Private;
+        public ICustomActivityResult Execute()
+        {
+            var message = string.Empty;
+            try
+            {
+                var credentials = new BasicAWSCredentials(AccessKey, SecretKey);
+                PutBucketRequest request = new PutBucketRequest()
+                {
+                    BucketName = bucketName,
+                    UseClientRegion = true,
+                };
+                var result = new AmazonS3Client(credentials, RegionEndpoint.USEast1).PutBucket(request);
+                if (result.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    message = "Success";
+                }
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null && (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId") || amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    message = "Please check the provided AWS Credentials.";
+                    message = message + "/nIf you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3";
+                }
+                else
+                {
+                    message = "An Error, number" + amazonS3Exception.ErrorCode + "occurred when creating a bucket with the message " + amazonS3Exception.Message;
+                }
+            }
+            return this.GenerateActivityResult(message);
+        }
+    }
+}
