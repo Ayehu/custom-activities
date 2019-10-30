1680fe9c1448a79ac3ae414e68ad6770bf2bbb28
Michael Pham <phamminh1309@gmail.com>
2019-10-30 14:27:15 +0700

    Add files via upload

---
+++ Amazon S3/AWSDeleteBucket/AWSDeleteBucket.cs
@@ -0,0 +1,26 @@
+﻿using Amazon;
+using Amazon.S3;
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Amazon.Runtime;
+namespace ayehu.Sdk.AWSS3
+{
+    class AWSDeleteBucket : IActivity
+    {
+        public string bucketName;
+        public string AccessKey;
+        public string SecretKey;
+
+        public ICustomActivityResult Execute()
+        {
+            var message = string.Empty;
+            var credentials = new BasicAWSCredentials(AccessKey, SecretKey);
+            using (var result = new AmazonS3Client(credentials, RegionEndpoint.USEast1))
+            {
+                result.DeleteBucket(bucketName);
+                message = "Success";
+            }
+            return this.GenerateActivityResult(message);
+        }
+    }
+}
