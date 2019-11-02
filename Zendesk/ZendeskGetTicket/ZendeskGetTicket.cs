67fbd4e265974820817abc57c126b03c5deb2e2c
zengboling <zengboling@gmail.com>
2019-11-02 17:27:48 +0800

    Create, Update,Get,Delete

---
+++ Zendesk/ZendeskGetTicket/ZendeskGetTicket.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Linq;
+using System.Net;
+using ZendeskApi_v2;
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+namespace Ayehu.Sdk.ActivityCreation
+{
+    public class ActivityClass : IActivity
+    {
+        public string Domain = null;
+        public string Username = null;
+        public string ApiToken = null;
+
+
+        public string TicketId = null;
+
+        public ICustomActivityResult Execute()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+        | SecurityProtocolType.Tls11
+        | SecurityProtocolType.Tls12
+        | SecurityProtocolType.Ssl3;
+            var api = new ZendeskApi(Domain, Username, ApiToken,"");
+            var res = api.Tickets.GetTicket(long.Parse(TicketId));
+            return this.GenerateActivityResult(SuccessResult(res.Ticket));
+
+        }
+
+       
+        private DataTable SuccessResult(object o)
+        {
+            DataTable dt = new DataTable("resultSet");
+
+            DataRow dr = dt.NewRow();
+            dt.Rows.Add(dr);
+            var filter = new string[] { "CollaboratorIds", "Tags", "CustomFields", "Via" };
+            o.GetType().GetProperties().ToList().ForEach(f =>
+            {
+                try
+                {
+                    if (!filter.Contains(f.Name))
+                    {
+                        f.GetValue(o, null);
+                        dt.Columns.Add(f.Name, f.PropertyType);
+                        dt.Rows[0][f.Name] = f.GetValue(o, null);
+                    }
+                }
+                catch { }
+            });
+            return dt;
+        }
+
+    }
+
+
+
+}
