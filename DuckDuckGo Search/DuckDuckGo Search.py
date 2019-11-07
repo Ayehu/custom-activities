c281d69e1b12c5f59c43847c3e4885bb5d240806
JackyLey <52428003+JackyLey@users.noreply.github.com>
2019-11-07 15:02:27 -0500

    Delete DuckDuckGo Search.py

--- DuckDuckGo Search/DuckDuckGo Search.py
+++
@@ -1,9 +0,0 @@
-import sys
-import urllib.request
-import urllib.parse
-
-def execute(Query):
-     Query = urllib.parse.quote(Query)
-     r = urllib.request.urlopen('https://api.duckduckgo.com/?q='+Query)
-     result = r.read()
-     return result
