55966602b742af5250d9e9e0d6177bfac4321f8f
vadim <wadimauss@yahoo.com>
2019-11-15 12:06:23 -0500

    Azure Check if a group exists.

---
+++ Azure Active Directory/AzureIsGroupExist/AzureIsGroupExist.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Data;
+using System.Linq;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+    public class AzureIsGroupExist : IActivity
+    {
+        /// <summary>
+        /// APPLICATION (CLIENT) ID
+        /// </summary>
+        public string appId;
+
+        /// <summary>
+        /// Directory (tenant) ID
+        /// </summary>
+        public string tenantId;
+
+        /// <summary>
+        /// Client secret
+        /// </summary>
+        /// <remarks>
+        /// A secret string that the application uses to prove its identity when requesting a token. 
+        /// Also can be referred to as application password.
+        /// </remarks>
+        public string secret;
+
+        /// <summary>
+        /// Group to check
+        /// </summary>
+        public string groupId;
+
+        public ICustomActivityResult Execute()
+        {
+            var auth = GetAuthenticated();
+            var group = auth.ActiveDirectoryGroups.List().Where(x => x.Id == groupId).FirstOrDefault();
+            DataTable dt = new DataTable("resultSet");
+            dt.Columns.Add("Result");
+
+            if (group != null)
+                dt.Rows.Add(true);
+            else
+                dt.Rows.Add(false);
+
+            return this.GenerateActivityResult(dt);
+        }
+
+        private Azure.IAuthenticated GetAuthenticated()
+        {
+            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(appId, secret, tenantId, AzureEnvironment.AzureGlobalCloud);
+            var azure = Azure
+                   .Configure()
+                   .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                   .Authenticate(credentials);
+
+            return azure;
+        }
+    }
+}
