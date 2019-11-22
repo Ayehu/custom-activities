da4ada48c01f7b54345ca0a3fc6e3b69465c473d
Vadim Visnevschi <vadim.visnevschi@bell.ca>
2019-11-22 11:33:14 -0500

    Improvements for UserResetPassword

--- Azure Active Directory/AzureADResetUserPassword/AzureADResetUserPassword.cs
+++ Azure Active Directory/AzureADResetUserPassword/AzureADResetUserPassword.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Data;
-using System.Linq;
 using Ayehu.Sdk.ActivityCreation.Interfaces;
 using Ayehu.Sdk.ActivityCreation.Extension;
 using Microsoft.Graph;
@@ -34,7 +33,7 @@ namespace Ayehu.Sdk.ActivityCreation
         /// New password
         /// </summary>
         public string password;
-        
+
         /// <summary>
         /// User email that identifies the account
         /// </summary>
@@ -42,18 +41,20 @@ namespace Ayehu.Sdk.ActivityCreation
 
         public ICustomActivityResult Execute()
         {
-            GraphServiceClient client = new GraphServiceClient("https://graph.microsoft.com/v1.0", GetProvider());
-            string userId = GetUserId(client);
-            User user = client.Users[userId].Request().GetAsync().Result;
+            GraphServiceClient client = new GraphServiceClient("https://graph.microsoft.com/v1.0", GetProvider());            
+            User user = client.Users[userEmail].Request().GetAsync().Result;
 
-            if (user.UserPrincipalName != null)
+            if (user != null && !string.IsNullOrEmpty(user.UserPrincipalName))
             {
                 var updateduser = new User();
 
                 if (!string.IsNullOrEmpty(password))
+                {
                     updateduser.PasswordProfile = new PasswordProfile { Password = password, ForceChangePasswordNextSignIn = false };
-
-                client.Users[userId].Request().UpdateAsync(updateduser).Wait();
+                    client.Users[userEmail].Request().UpdateAsync(updateduser).Wait();
+                }
+                else
+                    throw new Exception("Password can't be empty");
             }
             else
                 throw new Exception("User not found");
@@ -70,21 +71,6 @@ namespace Ayehu.Sdk.ActivityCreation
                 .Build();
 
             return new ClientCredentialProvider(confidentialClientApplication);
-        }
-
-        private string GetUserId(GraphServiceClient client)
-        {
-            var users = client.Users.Request().GetAsync().Result.ToList();
-
-            foreach (var user in users)
-            {
-                if (user.Mail != null && user.Mail.ToLower() == userEmail.ToLower())
-                {
-                    return user.Id;
-                }
-            }
-
-            return string.Empty;
         }
 
         private DataTable GetActivityResult
