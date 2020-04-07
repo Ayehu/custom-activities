47496459e4cfb4a57dee7c828023b67fbd0fcd96
yaronly <44620860+yaronly@users.noreply.github.com>
2020-04-07 10:04:34 -0400

    Add files via upload

---
+++ RSA Authentication Manager/RSAAMExtendUserToken/RSAAMExtendUserToken.vb
@@ -0,0 +1,133 @@
+Imports Ayehu.Sdk.ActivityCreation.Interfaces
+Imports Ayehu.Sdk.ActivityCreation.Extension
+Imports System.Text
+Imports System.Diagnostics
+Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System
+Imports System.Net
+Imports System.Net.Http
+Imports System.Net.Security
+Imports System.Security.Cryptography.X509Certificates
+Imports com.rsa.admin
+Imports com.rsa.admin.data
+Imports com.rsa.authmgr.admin.agentmgt
+Imports com.rsa.authmgr.admin.agentmgt.data
+Imports com.rsa.authmgr.admin.hostmgt.data
+Imports com.rsa.authmgr.admin.principalmgt
+Imports com.rsa.authmgr.admin.principalmgt.data
+Imports com.rsa.authmgr.admin.tokenmgt
+Imports com.rsa.authn
+Imports com.rsa.authn.data
+Imports com.rsa.common
+Imports com.rsa.command
+Imports com.rsa.command.exception
+Imports com.rsa.common.search
+Imports com.rsa.ucm.am
+Imports com.rsa.authmgr.admin.ondemandmgt
+Imports System.Data
+Imports com.rsa.authmgr.admin.tokenmgt.data
+
+Namespace Ayehu.Sdk.ActivityCreation
+    Public Class CustomActivity
+        Implements IActivity
+
+        Public HostName As String
+        Public UserName As String
+        Public Password As String
+        Private URL As String
+        Public AccountName As String
+        Public SecurityDomain As String
+        Public MDays As Integer
+        Private domain As SecurityDomainDTO
+        Private idSource As IdentitySourceDTO
+
+
+        Public Function Execute() As ICustomActivityResult Implements IActivity.Execute
+
+            'this will look for the token to be extened with the latest date from the one available.
+
+            UserName = UserName.Replace(HostName + "\", "")
+            URL = "https://" + HostName + ":7002/ims-ws/services/CommandServer"
+
+            ServicePointManager.Expect100Continue = True
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+
+            ServicePointManager.ServerCertificateValidationCallback =
+            New RemoteCertificateValidationCallback(AddressOf ValidateServerCertificate)
+
+            Dim conn As New SOAPCommandTarget(URL, UserName, Password)
+
+            If Not conn.Login(UserName, Password) Then Throw New System.Exception("Error: Unable to connect to the remote server. Please make sure your credentials are correct.")
+
+            CommandTargetPolicy.setDefaultCommandTarget(conn)
+
+            Dim searchRealmCmd As New SearchRealmsCommand
+            searchRealmCmd.filter = Filter.equal(RealmDTO._NAME_ATTRIBUTE, SecurityDomain)
+            searchRealmCmd.execute()
+
+            Dim realms() As RealmDTO
+            realms = searchRealmCmd.realms
+            If realms.Length = 0 Then Throw New System.Exception("Could not find realm " + SecurityDomain)
+
+            domain = realms(0).topLevelSecurityDomain
+            idSource = realms(0).identitySources(0)
+
+            Dim targetguid As String
+            Dim cmd As New ListTokensByPrincipalCommand
+            cmd.principalId = SearchUser()
+            If cmd.principalId = "" Then Throw New System.Exception("Unable to locate user " + AccountName)
+            Dim results() As ListTokenDTO
+            cmd.execute()
+            results = cmd.tokenDTOs
+            If results.Length = 0 Then Throw New System.Exception("Unable to locate token for user " + AccountName)
+            targetguid = results(0).guid
+
+            Dim TokentoUse As String
+            Dim newdate As DateTime = DateAdd(DateInterval.Day, MDays, DateTime.Now)
+
+            Dim cmd2 As New SearchTokensCommand
+            cmd2.filter = Filter.greater(ListTokenDTO._FILTER_EXPIRES_ON, newdate)
+            cmd2.limit = 1
+            cmd2.assigned = False
+            cmd2.execute()
+            results = cmd2.listSecurIDTokens
+            If results.Length = 0 Then Throw New System.Exception("Unable to locate available token")
+            TokentoUse = results(0).guid
+
+
+            Dim etk As New ExtendTokenDTO
+            etk.extendedTokenGuid = targetguid
+            etk.extentionTokenGuid = TokentoUse
+
+            Dim cm4 As New ExtendTokensCommand
+            cm4.extTknDTO = New ExtendTokenDTO() {etk}
+            cm4.execute()
+
+            Return GenerateActivityResult("Success")
+
+
+
+        End Function
+
+        Function SearchUser() As String
+
+            Dim cmd As New SearchPrincipalsIterativeCommand
+            cmd.limit = 1
+            cmd.identitySourceGuid = idSource.guid
+            cmd.filter = Filter.equal(PrincipalDTO._LOGINUID, AccountName)
+            Dim results() As PrincipalDTO
+            cmd.execute()
+            results = cmd.principals
+            If results.Length = 0 Then Return ""
+            Return results(0).guid
+
+        End Function
+
+        Function ValidateServerCertificate(ByVal sender As Object, ByVal certificate As X509Certificate,
+  ByVal chain As X509Chain, ByVal sslPolicyErrors As SslPolicyErrors) As Boolean
+            Return True
+        End Function
+
+    End Class
+End Namespace
---
+++ RSA Authentication Manager/readme.md
@@ -0,0 +1,6 @@
+RSA Authentication Manager V8.4 On-Prem.
+
+DLL's to reference
+
+rsaws-ci.dll
+System.Net.Http.dll
