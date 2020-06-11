d4a587be2efcae838a42032ea4a0a4c73fd8d802
Derek Pascarella <contact.pascarella@gmail.com>
2020-06-11 09:38:37 -0400

    Add files via upload

---
+++ ConvertDateToFileTime/ConvertDateToFileTime.cs
@@ -0,0 +1,24 @@
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System.Text;
+using System.Data;
+using System;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+    public class  CustomActivity: IActivity
+    {
+    	public string dateToConvert;
+        
+        public ICustomActivityResult Execute()
+        {
+        	var parsedDate = DateTime.Parse(dateToConvert);
+            
+            long dateFileTime = parsedDate.ToFileTime();
+            
+            var result = dateFileTime.ToString();
+            
+            return this.GenerateActivityResult(result);
+        }
+    }
+}
