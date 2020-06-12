94974e0557bffe83fa2f2aebb0f513cdc1de0b18
Derek Pascarella <contact.pascarella@gmail.com>
2020-06-11 22:52:32 -0400

    Add files via upload

---
+++ DownloadFile/DownloadFile.cs
@@ -0,0 +1,31 @@
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System.Text.RegularExpressions;
+using System.Net;
+using System;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+	public class  CustomActivity: IActivity
+	{
+		public string fileURL;
+		public string savePath;
+
+		public ICustomActivityResult Execute()
+		{
+			var result = "Success";
+
+			try
+			{
+				WebClient wc = new WebClient();
+				wc.DownloadFile(fileURL, savePath);
+			}
+			catch(WebException we)
+			{
+				result = "Error: " + we.ToString();
+			}
+
+			return this.GenerateActivityResult(result);
+		}
+	}
+}
