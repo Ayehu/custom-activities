b129af574a1a6c04c354b292fca3cdc3c91953ab
Derek Pascarella <contact.pascarella@gmail.com>
2020-06-12 23:04:51 -0400

    Add files via upload

---
+++ OpsGenie/OpsGenieDeleteUser/OpsGenieDeleteUser.cs
@@ -0,0 +1,60 @@
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
+using System.Net;
+using System;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+	public class CustomActivity : IActivity
+	{
+		public string apiKey;
+		public string userName;
+
+		public ICustomActivityResult Execute()
+		{
+			string apiURL = "https://api.opsgenie.com/v2/users/" + userName + "?teamIdentifierType=name";
+			string contentType = "application/json";
+			string accept = "application/json";
+			string method = "DELETE";
+
+			try
+			{
+				System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+				
+				var httpWebRequest = (HttpWebRequest)WebRequest.Create(apiURL);
+				httpWebRequest.ContentType = contentType;
+				httpWebRequest.Accept = accept;
+				httpWebRequest.Headers.Add("Authorization", "GenieKey " + apiKey);
+				httpWebRequest.Method = method;
+
+				var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+				using(var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+				{
+					var responseString = streamReader.ReadToEnd();
+
+					JObject jsonResults = JObject.Parse(responseString);
+					
+					string result = jsonResults["result"].ToString();
+
+					if(result == "Deleted")
+					{
+						return this.GenerateActivityResult("Success");
+					}
+					else
+					{
+						return this.GenerateActivityResult("Failure (" + result + ")");
+					}
+				}
+			}
+			catch(WebException e)
+			{
+				return this.GenerateActivityResult("Error (" + e.Message + ")");
+			}
+		}
+	}
+}
