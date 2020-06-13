0b82cd328151b37c33fab727951d41c422e754ac
Derek Pascarella <contact.pascarella@gmail.com>
2020-06-13 00:05:16 -0400

    Add files via upload

---
+++ OpsGenie/OpsGenieUserHasSchedules/OpsGenieUserHasSchedules.cs
@@ -0,0 +1,63 @@
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
+using System.Net;
+using System;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+	public class CustomActivity : IActivity
+	{
+		public string apiKey;
+		public string userName;
+		public string teamName;
+
+		public ICustomActivityResult Execute()
+		{
+			string apiURL = "https://api.opsgenie.com/v2/users/" + userName + "/schedules";
+			string contentType = "application/json";
+			string accept = "application/json";
+			string method = "GET";
+
+			try
+			{
+				System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+				
+				var httpWebRequest = (HttpWebRequest)WebRequest.Create(apiURL);
+				httpWebRequest.ContentType = contentType;
+				httpWebRequest.Accept = accept;
+				httpWebRequest.Headers.Add("Authorization", "GenieKey " + apiKey);
+				httpWebRequest.Method = method;
+
+				var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+				using(var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+				{
+					var responseString = streamReader.ReadToEnd();
+
+					JObject jsonResults = JObject.Parse(responseString);
+					
+					JArray schedules = (JArray)jsonResults["data"];
+
+					int scheduleCount = schedules.Count;
+
+					if(schedules.Count > 0)
+					{
+						return this.GenerateActivityResult("True");
+					}
+					else
+					{
+						return this.GenerateActivityResult("False");
+					}
+				}
+			}
+			catch(WebException e)
+			{
+				return this.GenerateActivityResult("Error (" + e.Message + ")");
+			}
+		}
+	}
+}
