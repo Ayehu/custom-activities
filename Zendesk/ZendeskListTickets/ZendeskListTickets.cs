b96d6fa3db635866b918b173529c82c2121858d2
Derek Pascarella <contact.pascarella@gmail.com>
2020-08-26 15:28:27 -0400

    Added result sorting and ordering.

--- Zendesk/ZendeskListTickets/ZendeskListTickets.cs
+++ Zendesk/ZendeskListTickets/ZendeskListTickets.cs
@@ -8,6 +8,7 @@ using ZendeskApi_v2;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ZendeskApi_v2.Models.Tickets;
+
 namespace Ayehu.Sdk.ActivityCreation
 {
     public class ActivityClass : IActivity
@@ -16,6 +17,8 @@ namespace Ayehu.Sdk.ActivityCreation
         public string Username = null;
         public string ApiToken = null;
         public string Page = null;
+        public string SortBy = null;
+        public string OrderBy = null;
         public string Type = null;
         public string Priority = null;
         public string Status = null;
@@ -50,29 +53,37 @@ namespace Ayehu.Sdk.ActivityCreation
             var termType = string.IsNullOrEmpty(Type) ? "" : "ticket_type:" + Type + " ";
             var termPriority = string.IsNullOrEmpty(Priority) ? "" : "priority:" + Priority + " ";
             var termStatus = string.IsNullOrEmpty(Status) ? "" : "status:" + Status + " ";
-            Term = (Query + " " + termStatus + termPriority + termType).Trim();
+            var termSortBy = string.IsNullOrEmpty(SortBy) ? "" : "&sort_by=" + SortBy + " ";
+            var termOrderBy = string.IsNullOrEmpty(OrderBy) ? "" : "&order_by=" + OrderBy + " ";
+            
+            Term = (Query + " " + termStatus + termPriority + termType + termSortBy + termOrderBy).Trim();
             
             var pageRes = api.Search.SearchFor<Ticket>(Term);
             TotalPage = pageRes.TotalPages;
 
             var res = api.Search.SearchFor<Ticket>(Term, page: 10);
             var tickets = GetTickets();
+
             return this.GenerateActivityResult(ItemsToDatatable(tickets));
         }
+
         private List<Ticket> GetTickets()
         {
             var page = int.Parse(string.IsNullOrEmpty(Page) ? "1" : Page);
             var tickets = new List<Ticket>();
+            
             for (var i = (page - 1) * 10 + 1; i <= page * 10; i++)
             {
                 if (i > TotalPage)
                     break;
+                
                 var res = api.Search.SearchFor<Ticket>(Term, page: i);
                 tickets.AddRange(res.Results);
             }
 
             return tickets;
         }
+
         private DataTable ItemsToDatatable(IEnumerable<object> items)
         {
             DataTable mergedTable = null;
@@ -84,11 +95,13 @@ namespace Ayehu.Sdk.ActivityCreation
                 var dt = new DataTable("resultSet");
                 var row = dt.NewRow();
                 dt.Rows.Add(row);
+                
                 foreach (var pair in jObject)
                 {
                     dt.Columns.Add(pair.Key);
                     dt.Rows[0][pair.Key] = pair.Value.ToString();
                 }
+                
                 if (mergedTable == null)
                     mergedTable = dt;
                 else
@@ -96,16 +109,15 @@ namespace Ayehu.Sdk.ActivityCreation
                     mergedTable.Merge(dt, true, MissingSchemaAction.Add);
                 }
             }
+
             if (mergedTable == null)
             {
                 mergedTable = new DataTable("resultSet");
                 mergedTable.Columns.Add("Result", typeof(string));
                 mergedTable.Rows.Add("0");
             }
+
             return mergedTable;
         }
     }
-
-
-
 }
