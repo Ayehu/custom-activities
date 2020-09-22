1cec699c622d3ae9b96f13e0972690cbec5d821c
Derek Pascarella <contact.pascarella@gmail.com>
2020-09-21 21:55:49 -0400

    Add files via upload

--- Azure/AzureGetResourceInfo/AzureGetResourceInfo.cs
+++ Azure/AzureGetResourceInfo/AzureGetResourceInfo.cs
@@ -52,9 +52,9 @@ namespace AzureGetResourceInfo
             var credential = new ClientCredential(clientId, clientSecret);
             var result = authenticationContext.AcquireTokenAsync(resource: "https://management.azure.com/", clientCredential: credential).Result;
 
-            if (result == null)
+            if(result == null)
             {
-                return this.GenerateActivityResult("Failed to obtain JWT token.");
+                throw new Exception("Failed to obtain JWT token.");
             }
 
             string token = result.AccessToken;
@@ -89,7 +89,7 @@ namespace AzureGetResourceInfo
             }
             catch(WebException e)
             {
-                return this.GenerateActivityResult("Error (" + e.Message + ")");
+                throw new Exception(e.Message);
             }
             
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://management.azure.com" + resourceId + "?api-version=" + latestAPI);
@@ -125,7 +125,7 @@ namespace AzureGetResourceInfo
             }
             catch(WebException e)
             {
-                return this.GenerateActivityResult("Error (" + e.Message + ")");
+                throw new Exception( e.Message);
             }
         }
     }
