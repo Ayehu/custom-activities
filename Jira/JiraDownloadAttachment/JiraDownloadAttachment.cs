0f7c9f8112188d2ea2fed0f4d7c93b216fc9bc86
Derek Pascarella <contact.pascarella@gmail.com>
2020-10-06 12:24:02 -0400

    Add files via upload

---
+++ Jira/JiraDownloadAttachment/JiraDownloadAttachment.cs
@@ -0,0 +1,55 @@
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System;
+
+namespace Ayehu.Sdk.ActivityCreation
+{
+	public class CustomActivity: IActivity
+	{
+		public string instanceURL;
+		public string username;
+		public string apiToken;
+		public string attachmentID;
+		public string attachmentFilename;
+		public string savePath;
+		public string customFilename;
+		public int customFilenameBoolean;
+
+		public ICustomActivityResult Execute()
+		{
+			try
+			{
+				WebClient wc = new WebClient();
+
+				wc.Headers["Authorization"] = "Basic " + System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + apiToken));
+				wc.Headers["X-Atlassian-Token"] = "nocheck";
+
+				string apiURL = instanceURL + "/secure/attachment/" + attachmentID + "/" + attachmentFilename;
+				
+				if(customFilenameBoolean == 1)
+				{
+					savePath += "/" + customFilename;
+				}
+				else
+				{
+					savePath += "/" + attachmentFilename;
+				}
+
+				wc.DownloadFile(apiURL, savePath);
+
+				return this.GenerateActivityResult("Success");
+			}
+			catch(WebException we)
+			{
+				throw new Exception(we.Message);
+			}
+		}
+	}
+}
