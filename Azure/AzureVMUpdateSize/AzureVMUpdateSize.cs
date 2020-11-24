59ec441231f0ac918a321d8b4fd17fe9d59ce2ed
Derek Pascarella <contact.pascarella@gmail.com>
2020-11-24 11:45:30 -0500

    Add files via upload

---
+++ Azure/AzureVMUpdateSize/AzureVMUpdateSize.cs
@@ -0,0 +1,138 @@
+using System;
+using Ayehu.Sdk.ActivityCreation.Interfaces;
+using Ayehu.Sdk.ActivityCreation.Extension;
+using Ayehu.Sdk.ActivityCreation.Helpers;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Ayehu.Azure_VM
+{
+    public class AzureVMUpdateSize : IActivityAsync
+    {
+    
+    public string Jsonkeypath = "";
+    public string authToken_password = "";
+    public string resourceGroupName = "";
+	public string subscriptionId = "";
+    public string api_version = "";
+    public string vm_name = "";
+    public string vm_size = "";
+	
+    private bool omitJsonEmptyorNull = true;
+    private string contentType = "application/json";    
+    private string endPoint = "https://management.azure.com";
+    private string httpMethod = "PATCH";
+    private string _uriBuilderPath;
+    private string _postData;
+    
+    private System.Collections.Generic.Dictionary<string, string> _headers;    
+    private System.Collections.Generic.Dictionary<string, string> _queryStringArray;
+    
+    private string uriBuilderPath {
+        get {
+            if (string.IsNullOrEmpty(_uriBuilderPath)) {
+_uriBuilderPath = string.Format("/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Compute/virtualMachines/{2}",subscriptionId,resourceGroupName,vm_name);
+            }
+return _uriBuilderPath;
+        }
+        set {
+            this._uriBuilderPath = value;
+        }
+    }
+    
+    private string postData {
+        get {
+            if (string.IsNullOrEmpty(_postData)) {
+                //_postData = "{ \"location\": \"" + disk_location + "\", \"properties\": { \"creationData\": { \"createOption\": \"Empty\" }, \"diskSizeGB\": " + disk_size + " } }";
+                _postData = "{ \"properties\": { \"hardwareProfile\": { \"vmSize\": \"" + vm_size + "\" } } }";
+            }
+return _postData;
+        }
+        set {
+            this._postData = value;
+        }
+    }
+    
+    private System.Collections.Generic.Dictionary<string, string> headers {
+        get {
+            if (_headers == null) {
+_headers = new Dictionary<string, string>() { {"authorization","Bearer " + authToken_password} };
+            }
+return _headers;
+        }
+        set {
+            this._headers = value;
+        }
+    }
+    
+    private System.Collections.Generic.Dictionary<string, string> queryStringArray {
+        get {
+            if (_queryStringArray == null) {
+_queryStringArray = new Dictionary<string, string>() {{"api-version",api_version} };
+            }
+return _queryStringArray;
+        }
+        set {
+            this._queryStringArray = value;
+        }
+    }
+    
+        public async System.Threading.Tasks.Task<ICustomActivityResult> Execute()
+        {
+
+            HttpClient client = new HttpClient();
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+            UriBuilder UriBuilder = new UriBuilder(endPoint); 
+            UriBuilder.Path = uriBuilderPath;
+            UriBuilder.Query = AyehuHelper.queryStringBuilder(queryStringArray);
+            HttpRequestMessage myHttpRequestMessage = new HttpRequestMessage(new HttpMethod(httpMethod), UriBuilder.ToString());
+           
+            if (contentType == "application/x-www-form-urlencoded")
+                myHttpRequestMessage.Content = AyehuHelper.formUrlEncodedContent(postData);
+            else
+              if (string.IsNullOrEmpty(postData) == false)
+                if (omitJsonEmptyorNull)
+                    myHttpRequestMessage.Content = new StringContent(AyehuHelper.omitJsonEmptyorNull(postData), Encoding.UTF8, "application/json");
+                else
+                    myHttpRequestMessage.Content = new StringContent(postData, Encoding.UTF8, contentType);
+
+
+            foreach (KeyValuePair<string, string> headeritem in headers)
+                client.DefaultRequestHeaders.Add(headeritem.Key, headeritem.Value);
+
+            HttpResponseMessage response = client.SendAsync(myHttpRequestMessage).Result;
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.NoContent:
+                case HttpStatusCode.Created:
+                case HttpStatusCode.Accepted:
+                case HttpStatusCode.OK:
+                    {
+                        if (string.IsNullOrEmpty(response.Content.ReadAsStringAsync().Result) == false)
+                            return this.GenerateActivityResult(response.Content.ReadAsStringAsync().Result, Jsonkeypath);
+                        else
+                            return this.GenerateActivityResult("Success");
+                    }
+                default:
+                    {
+                        if (string.IsNullOrEmpty(response.Content.ReadAsStringAsync().Result) == false)
+                            throw new Exception(response.Content.ReadAsStringAsync().Result);
+                        else if (string.IsNullOrEmpty(response.ReasonPhrase) == false)
+                            throw new Exception(response.ReasonPhrase);
+                        else
+                            throw new Exception(response.StatusCode.ToString());
+                    }
+            }
+        }
+
+        public bool AcceptAllCertifications(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certification, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
