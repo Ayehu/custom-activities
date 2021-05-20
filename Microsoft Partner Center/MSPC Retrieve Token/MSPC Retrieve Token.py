32d7c05cc5525508807110aa5e4ffaba98fc42af
Derek Pascarella <contact.pascarella@gmail.com>
2021-05-20 17:21:01 -0400

    Add files via upload

--- Microsoft Partner Center/MSPC Retrieve Token/MSPC Retrieve Token.py
+++ Microsoft Partner Center/MSPC Retrieve Token/MSPC Retrieve Token.py
@@ -7,21 +7,15 @@ def execute(Resource,TenantID,ClientID,password,GrantType):
     'Content-Type': 'application/x-www-form-urlencoded'
   }
 
-  resource = Resource
-  tenant_id = TenantID
-  client_id = ClientID
-  client_secret = password
-  grant_type = GrantType
-
   auth_body_data = {
-    'resource': resource,
-    'grant_type': grant_type,
-    'client_secret': client_secret,
-    'client_id' : client_id
+    'resource': Resource,
+    'grant_type': GrantType,
+    'client_secret': password,
+    'client_id' : ClientID
   }
 
   try:
-      access_token_response = requests.post("https://login.microsoftonline.com/" + tenant_id + "/oauth2/token", data=auth_body_data, allow_redirects=False)
+      access_token_response = requests.post("https://login.microsoftonline.com/" + TenantID + "/oauth2/token", data=auth_body_data, allow_redirects=False)
       access_token_response.raise_for_status()
   except requests.exceptions.HTTPError as errh:
       result = (errh)
